823fee092c9947f4b70beb8f1804ae8b8c672a29
davidhsu <davidhsu@SN-2UA62724XY.wheaton.edu>
2017-06-08 13:50:10 -0500

    master test

--- percentage_crystallinity_git.m
+++ percentage_crystallinity_git.m
@@ -135,6 +135,7 @@ zlabel('z')
 % This is a github test for Matlab integration
 % This is the second github test for Matlab integration
 % This is the 3rd test
+% Master test
 
 % Sort vector IDs into bins along X-direction
 xbins=10;
